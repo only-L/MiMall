6a477b9941a87cf5c8a33005a9f691a2eb393981
only-L <1037075694@qq.com>
2020-02-03 12:37:53 +0800

    学习CORS跨域

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  mounted() {
+    let url =
+      'https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list'
+    axios.get(url).then(() => {})
   }
 }
 </script>
