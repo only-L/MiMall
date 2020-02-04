a01ae468d8e1575d5cd6b068b75a04e9a04bca97
only-L <1037075694@qq.com>
2020-02-04 20:30:57 +0800

    导航头组件开发

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -25,7 +25,38 @@
     </div>
     <!-- 灰色头部--end -->
 
-    <div class="nav-header"></div>
+    <!-- 导航菜单 -->
+    <div class="nav-header">
+      <!-- 容器 -->
+      <div class="container">
+        <!-- logo -->
+        <div class="header-logo">
+          <a href="/#/index"></a>
+        </div>
+        <!-- 菜单 -->
+        <div class="header-menu">
+          <div class="item-menu">
+            <span>小米手机</span>
+            <div class="children"></div>
+          </div>
+          <div class="item-menu">
+            <span>RedMi红米</span>
+            <div class="children"></div>
+          </div>
+          <div class="item-menu">
+            <span>电视</span>
+            <div class="children"></div>
+          </div>
+        </div>
+        <!-- 搜索框 -->
+        <div class="header-search">
+          <div class="wrapper">
+            <input type="text" name="keyword" />
+            <a href="javascript:;"></a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -36,20 +67,16 @@ export default {
 </script>
 
 <style lang="scss">
+@import './../assets/scss/base.scss';
+@import './../assets/scss/mixin.scss';
 .header {
   .nav-topbar {
     height: 39px;
     line-height: 39px;
     background-color: #333333;
-
     .container {
-      width: 1226px;
-      margin-left: auto;
-      margin-right: auto;
       // 两边对齐
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
+      @include flex();
       a {
         display: inline-block;
         color: #b0b0b0;
@@ -61,13 +88,99 @@ export default {
         text-align: center;
         color: #ffffff;
         .icon-cart {
-          display: inline-block;
+          @include bgImg(16px,12px,'/imgs/icon-cart-checked.png');
+          /* display: inline-block;
           width: 16px;
           height: 12px;
           background: url('/imgs/icon-cart-checked.png') no-repeat center;
           // 把图像图像扩展至最大尺寸，以使其宽度和高度完全适应内容区域
-          background-size: contain;
+          background-size: contain; */
           margin-right: 4px;
+        }
+      }
+    }
+  }
+  .nav-header {
+    .container {
+      height: 112px;
+      @include flex();
+      .header-logo {
+        display: inline-block;
+        width: 55px;
+        height: 55px;
+        background-color: #ff6600;
+        a {
+          display: inline-block;
+          width: 110px;
+          height: 55px;
+          // scss伪类选择器
+          &:before {
+            content: '';
+            @include bgImg(55px,55px,'/imgs/mi-logo.png',55px);
+            /* display: inline-block;
+            width: 55px;
+            height: 55px;
+            background: url('/imgs/mi-logo.png') no-repeat center;
+            background-size: 55px; */
+            transition: margin 0.2s;
+          }
+          &:after {
+            content: '';
+            @include bgImg(55px,55px,'/imgs/mi-home.png',55px);
+            /* display: inline-block;
+            width: 55px;
+            height: 55px;
+            background: url('/imgs/mi-home.png') no-repeat center;
+            background-size: 55px; */
+          }
+          &:hover:before {
+            margin-left: -55px;
+            transition: margin 0.2s;
+          }
+        }
+      }
+      .header-menu {
+        display: inline-block;
+        width: 643px;
+        padding-left: 209px;
+        .item-menu {
+          display: inline-block;
+          color: #333333;
+          font-weight: bold;
+          font-size: 16px;
+          line-height: 112px;
+          span {
+            cursor: pointer;
+          }
+          &:hover {
+          }
+        }
+      }
+      .header-search {
+        width: 319px;
+        .wrapper {
+          height: 50px;
+          border: 1px solid #ff6600;
+          display: flex;
+          align-items: center;
+          input {
+            border: none;
+            // 为input指定宽度和高度
+            box-sizing: border-box;
+            border-right: 1px solid #ff6600;
+            width: 264px;
+            height: 50px;
+            padding: 0 14px;
+          }
+          a {
+            @include bgImg(18px,18px,'/imgs/icon-search.png');
+            /* display: inline-block;
+            width: 18px;
+            height: 18px;
+            background: url('/imgs/icon-search.png') no-repeat center;
+            background-size: contain; */
+            margin-left: 17px;
+          }
         }
       }
     }
