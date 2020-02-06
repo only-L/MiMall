3bdb26ae6359fa61ea1d7833583bce4cf76daae6
only-L <1037075694@qq.com>
2020-02-06 18:35:42 +0800

    首页轮播图

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,138 @@
 <template>
-  <div>
+  <div class="index">
+    <div class="container">
+      <!-- 轮播图盒子 -->
+      <div class="swiper-box">
+        <!-- 菜单 -->
+        <div class="nav-menu">
+          <ul class="menu-wrap">
+            <li class="menu-item">
+              <a href="javascript:;">手机 电话卡</a>
+              <div class="children">
+                <ul v-for="(item,i) in menuList" v-bind:key="i">
+                  <li v-for="(sub,j) in item" v-bind:key="j">
+                    <a v-bind:href="sub?'/#/product/'+sub.id:''">
+                      <img v-bind:src="sub?sub.img:'/imgs/item-box-1.png'" alt />
+                      {{sub?sub.name:'小米9'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电视 盒子</a>
+              <div class="children">
+                <ul v-for="(item,i) in menuList" v-bind:key="i">
+                  <li v-for="(sub,j) in item" v-bind:key="j">
+                    <a v-bind:href="sub?'/#/product/'+sub.id:''">
+                      <img v-bind:src="sub?sub.img:'/imgs/item-box-5.webp'" alt />
+                      {{sub?sub.name:'小米电视5 55英寸'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">笔记本 显示器 平板</a>
+              <div class="children">
+                <ul v-for="(item,i) in menuList" v-bind:key="i">
+                  <li v-for="(sub,j) in item" v-bind:key="j">
+                    <a v-bind:href="sub?'/#/product/'+sub.id:''">
+                      <img v-bind:src="sub?sub.img:'/imgs/item-box-6.webp'" alt />
+                      {{sub?sub.name:'RedmiBook 13'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">家电 插线板</a>
+              <div class="children">
+                <ul v-for="(item,i) in menuList" v-bind:key="i">
+                  <li v-for="(sub,j) in item" v-bind:key="j">
+                    <a v-bind:href="sub?'/#/product/'+sub.id:''">
+                      <img v-bind:src="sub?sub.img:'/imgs/item-box-1.png'" alt />
+                      {{sub?sub.name:'小米9'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">出行 穿戴</a>
+              <div class="children">
+                <ul v-for="(item,i) in menuList" v-bind:key="i">
+                  <li v-for="(sub,j) in item" v-bind:key="j">
+                    <a v-bind:href="sub?'/#/product/'+sub.id:''">
+                      <img v-bind:src="sub?sub.img:'/imgs/item-box-1.png'" alt />
+                      {{sub?sub.name:'小米9'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">智能 路由器</a>
+              <div class="children">
+                <ul v-for="(item,i) in menuList" v-bind:key="i">
+                  <li v-for="(sub,j) in item" v-bind:key="j">
+                    <a v-bind:href="sub?'/#/product/'+sub.id:''">
+                      <img v-bind:src="sub?sub.img:'/imgs/item-box-1.png'" alt />
+                      {{sub?sub.name:'小米9'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电源 配件</a>
+              <div class="children">
+                <ul v-for="(item,i) in menuList" v-bind:key="i">
+                  <li v-for="(sub,j) in item" v-bind:key="j">
+                    <a v-bind:href="sub?'/#/product/'+sub.id:''">
+                      <img v-bind:src="sub?sub.img:'/imgs/item-box-1.png'" alt />
+                      {{sub?sub.name:'小米9'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">健康 儿童</a>
+              <div class="children">
+                <ul v-for="(item,i) in menuList" v-bind:key="i">
+                  <li v-for="(sub,j) in item" v-bind:key="j">
+                    <a v-bind:href="sub?'/#/product/'+sub.id:''">
+                      <img v-bind:src="sub?sub.img:'/imgs/item-box-1.png'" alt />
+                      {{sub?sub.name:'小米9'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <!-- 轮播图 -->
+        <swiper :options="swiperOption">
+          <swiper-slide :key="index" v-for="(item,index) in slideList">
+            <a :href="'/#/product/'+item.id">
+              <img :src="item.img" />
+            </a>
+          </swiper-slide>
+          <!-- 分页器 -->
+          <div class="swiper-pagination" slot="pagination"></div>
+          <!-- 导航按钮 -->
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </div>
+      <!-- 四个图 -->
+      <div class="abx-box"></div>
+      <!-- 一张图 -->
+      <div class="bananer"></div>
+      <!-- 手机列表 -->
+      <div class="product-box"></div>
+    </div>
     <ServiceBar></ServiceBar>
   </div>
 </template>
@@ -7,13 +140,191 @@
 <script>
 // 添加组件
 import ServiceBar from './../components/ServiceBar'
+// swiper轮播图插件
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 export default {
   name: 'index',
   components: {
+    swiper,
+    swiperSlide,
     ServiceBar
+  },
+  data() {
+    return {
+      // 所有的参数同 swiper 官方 api 参数
+      swiperOption: {
+        // 自动播放
+        autoplay: true,
+        // 循环
+        loop: true,
+        // 切换效果
+        effect: 'cube',
+        cubeEffect: {
+          slideShadows: true,
+          shadow: true,
+          shadowOffset: 100,
+          shadowScale: 0.6
+        },
+        // 上下一页
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        }
+      },
+      // 轮播图片的数组
+      slideList: [
+        {
+          id: '42',
+          img: '/imgs/slider/slide-1.jpg'
+        },
+        {
+          id: '45',
+          img: '/imgs/slider/slide-2.jpg'
+        },
+        {
+          id: '46',
+          img: '/imgs/slider/slide-3.jpg'
+        },
+        {
+          id: '',
+          img: '/imgs/slider/slide-4.jpg'
+        },
+        {
+          id: '',
+          img: '/imgs/slider/slide-5.jpg'
+        }
+      ],
+      menuList: [
+        [
+          {
+            id: 30,
+            img: '/imgs/item-box-1.png',
+            name: '小米CC9'
+          },
+          {
+            id: 31,
+            img: '/imgs/item-box-2.png',
+            name: '小米8青春版'
+          },
+          {
+            id: 32,
+            img: '/imgs/item-box-3.jpg',
+            name: 'Redmi k20 Pro'
+          },
+          {
+            id: 33,
+            img: '/imgs/item-box-4.jpg',
+            name: '移动4G专区'
+          }
+        ],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ]
+    }
   }
 }
 </script>
 
 <style lang="scss">
+@import './../assets/scss/config.scss';
+@import './../assets/scss/mixin.scss';
+.index {
+  .swiper-box {
+    // 左列表
+    .nav-menu {
+      position: absolute;
+      width: 264px;
+      height: 451px;
+      z-index: 9;
+      background: rgba(105, 101, 101, 0.6);
+      padding: 26px 0;
+      // 使盒子模型不受padding影响
+      box-sizing: border-box;
+      .menu-wrap {
+        // padding: 26px 0;
+        .menu-item {
+          height: 50px;
+          line-height: 50px;
+          a {
+            font-size: 16px;
+            color: #ffffff;
+            font-weight: bold;
+            padding: 0 0 0 30px;
+            position: relative;
+            display: block;
+            &:after {
+              content: '';
+              @include bgImg(10px, 15px, '/imgs/icon-arrow.png');
+              position: absolute;
+              top: 17.5px;
+              right: 20px;
+              color: #e0e0e0;
+            }
+          }
+          &:hover {
+            background-color: #ff6600;
+            .children {
+              display: block;
+            }
+          }
+          .children {
+            display: none;
+            width: 962px;
+            height: 449px;
+            background-color: $colorG;
+            position: absolute;
+            top: 0;
+            left: 264px;
+            border: 1px solid #e5e5e5;
+            ul {
+              display: flex;
+              justify-content: space-between;
+              height: 75px;
+              li {
+                height: 75px;
+                line-height: 75px;
+                flex: 1;
+                padding-left: 23px;
+                a {
+                  color: $colorB;
+                  font-size: 14px;
+                }
+                img {
+                  width: 42px;
+                  height: 35px;
+                  vertical-align: middle;
+                  margin-right: 15px;
+                }
+                &:hover {
+                  a {
+                    color: $colorA;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    // 轮播
+    .swiper-container {
+      height: 451px;
+      .swiper-button-prev {
+        left: 274px;
+      }
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+}
 </style>
